9792d83b0c0c113c7dcd6b87128403174cd7975c
liantsoa17 <liantsurakotonirina@gmail.com>
2024-02-07 09:51:34 +0300

    Login

--- controllers/UtilisateurController.js
+++ controllers/UtilisateurController.js
@@ -18,14 +18,14 @@ const inscription= async (req, res, next) => {
 
     //Vérification champ vide
     if (!nom || !prenom|| !contact || !sexe || !mdp || !email) {
-      return res.status(400).json({ error: 'Le remplissage de tous les champs est requis' });
+      return res.status(400).json({ message: 'Le remplissage de tous les champs est requis' });
     }
 
     // Vérification date
     const today = new Date();
     const dateNaissance = new Date(date_naissance);
     if (dateNaissance >= today) {
-      return res.status(400).json({ error: 'Date de naissance invalide' });
+      return res.status(400).json({ message: 'Date de naissance invalide' });
     }
 
     
@@ -36,7 +36,7 @@ const inscription= async (req, res, next) => {
     await utilisateur.save();
     res.status(201).json({ message: 'Utilisateur bien enregistrer' });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -45,17 +45,17 @@ const login= async (req, res, next) => {
     const { email, mdp } = req.body;
     const utilisateur = await UtilisateurModel.findOne({ email });
     if (!utilisateur) {
-      return res.status(401).json({ error: 'email invalide' });
+      return res.status(401).json({ message: 'email invalide' });
     }
     const mdpValide = await bcrypt.compare(mdp, utilisateur.mdp);
     if (!mdpValide) {
-      return res.status(401).json({ error: 'mot de passe invalide' });
+      return res.status(401).json({ message: 'mot de passe invalide' });
     }
     const token = jwt.sign({ userId: utilisateur._id }, 'salon', { expiresIn: 86400 });
-    res.status(200).json({ utilisateur: utilisateur, token: token });    
+    res.status(200).json({ status: "200",message: 'Vous êtes connecté', token: token });    
     //res.json({ token });
   } catch (error) {
-    res.status(500).json({ error: 'Error logging in' });
+    res.status(500).json({ message: 'Erreur de connexion' });
   }
   
 };
@@ -64,17 +64,17 @@ const verification = async(req, res, next) => {
   const token = req.query.token;
 
   if (!token) {
-    return res.status(401).json({ error: 'Aucun token' });
+    return res.status(401).json({message: 'Aucun token' });
   }
  
 
   jwt.verify(token, 'salon' , (err, decoded) => {
     if(err){
       if (err.name === 'TokenExpiredError') {
-        return res.status(401).json({ error: 'Token expirée '+token });
+        return res.status(401).json({message: 'Token expirée '+token });
       } 
       else {
-        return res.status(401).json({ error: 'Token non valide' });
+        return res.status(401).json({ message: 'Token non valide' });
       }
     }
     else{
--- routes/Router.js
+++ routes/Router.js
@@ -7,7 +7,7 @@ const utilisateur_controller=require('../controllers/UtilisateurController');
 
 router.get('/utilisateur',utilisateur_controller.getUtilisateur);
 router.post('/inscription',utilisateur_controller.inscription);
-router.post('/login',utilisateur_controller.login);
+router.post('/user/login',utilisateur_controller.login);
 
 
 module.exports = router;

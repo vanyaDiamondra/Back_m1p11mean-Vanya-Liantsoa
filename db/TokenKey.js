5ea74072a255dab1c862af81abfa1eb46039c907
Vanya diamondra <vanyadiamondra@gmail.com>
2024-02-29 23:04:22 +0000

    update url

--- db/TokenKey.js
+++ db/TokenKey.js
@@ -1,6 +1,6 @@
 const secretKey = 'salon';
 const user='aureliekelen@gmail.com' ;
 const pass='amalmlcqbqvdaimf'
-const base_url='http://localhost:4200'
+const base_url='https://front-m1p11mean-vanya-liantsoa.onrender.com/'
 
 module.exports = {secretKey,user,pass,base_url}
